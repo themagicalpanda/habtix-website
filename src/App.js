40736e52a88b6d492f3d55b93ee3589ab5412f82
themagicalpanda <vipulkodle025@gmail.com>
2023-12-15 13:47:40 +0530

    Created under construction placeholder website page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import image from "./undraw_under_construction_-46-pa.svg";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Habtix is gonna be live soon!</h1>
+    <div className="fullscreen-container ">
+      <img src={image} alt="Construction img" />
+      <h1 className="font-semibold text-5xl text-rose-200">Habtix will be live soon!</h1>
     </div>
   );
 }
